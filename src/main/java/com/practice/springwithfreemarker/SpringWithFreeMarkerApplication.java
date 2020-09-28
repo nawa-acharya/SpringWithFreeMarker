f5050a877c639f8b8401edc9027cfcd63030cf3a
Nawa Acharya <nawa@marathoncs.com>
2020-09-28 10:27:56 -0700

    Created a new project for practicing spring with free marker

---
+++ src/main/java/com/practice/springwithfreemarker/SpringWithFreeMarkerApplication.java
@@ -0,0 +1,13 @@
+package com.practice.springwithfreemarker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithFreeMarkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWithFreeMarkerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/practice/springwithfreemarker/SpringWithFreeMarkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.springwithfreemarker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithFreeMarkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
